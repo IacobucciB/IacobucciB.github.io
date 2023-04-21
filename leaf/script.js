ce4e0343ddd6922b69fe0af048b333351569ace8
Iacobucci Juan B <32987645+IacobucciB@users.noreply.github.com>
2023-04-21 12:53:16 -0300

    Two way animation

--- leaf/script.js
+++ leaf/script.js
@@ -1,36 +1,36 @@
-// Configuración inicial del mapa
 let config = {
   minZoom: 7,
   maxZoom: 18,
 }
 const zoom = 12
 
-// Coordenadas de los marcadores
 const marker1Lat = -34.922883
 const marker1Lng = -57.956317
 
 const marker2Lat = -54.801912
 const marker2Lng = -68.303216
 
-// Crear el mapa
 const map = L.map("map", config).setView([marker1Lat, marker1Lng], zoom)
 
-// Añadir capa de OpenStreetMap
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map)
 
-// Crear los marcadores
 const marker1 = L.marker([marker1Lat, marker1Lng]).addTo(map)
 const marker2 = L.marker([marker2Lat, marker2Lng]).addTo(map)
+marker2.bindPopup("<b>Otro Lorem Ipsum</b><br>Maecenas euismod cursus dignissim.")
 
-// Función para animar el mapa desde el marcador 1 al marcador 2
-function animateMap() {
-  map.flyTo([marker2Lat, marker2Lng], 15) // Zoom al marcador 2
-  marker2.bindPopup("<b>Otro Lorem Ipsum</b><br>Maecenas euismod cursus dignissim.").openPopup() // Abrir el pop-up del marcador 2
-}
-
-// Agregar evento al botón para animar el mapa
-const animateButton = document.getElementById("animate-button")
-animateButton.addEventListener("click", animateMap)
+let currentMarker = marker1
+const animateButton = document.getElementById("animateButton")
+animateButton.addEventListener("click", () => {
+  if (currentMarker === marker1) {
+    map.flyTo([marker2Lat, marker2Lng], 15)
+    currentMarker = marker2
+    animateButton.textContent = "Ir a Marker 1"
+  } else {
+    map.flyTo([marker1Lat, marker1Lng], zoom)
+    currentMarker = marker1
+    animateButton.textContent = "Ir a Marker 2"
+  }
+})
