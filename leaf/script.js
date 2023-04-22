be586f5ec1d72bdf4168cddd82568235529246d6
IacobucciB <link00222@gmail.com>
2023-04-21 23:17:43 -0300

    Better button logic

--- leaf/script.js
+++ leaf/script.js
@@ -4,33 +4,29 @@ let config = {
 }
 const zoom = 12
 
-const marker1Lat = -34.922883
-const marker1Lng = -57.956317
+const markers = [
+  { lat: -34.922883, lng: -57.956317 },
+  { lat: -54.801912, lng: -68.303216 },
+]
 
-const marker2Lat = -54.801912
-const marker2Lng = -68.303216
-
-const map = L.map("map", config).setView([marker1Lat, marker1Lng], zoom)
+const map = L.map("map", config).setView([markers[0].lat, markers[0].lng], zoom)
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map)
 
-const marker1 = L.marker([marker1Lat, marker1Lng]).addTo(map)
-const marker2 = L.marker([marker2Lat, marker2Lng]).addTo(map)
-marker2.bindPopup("<b>Otro Lorem Ipsum</b><br>Maecenas euismod cursus dignissim.")
+const leafletMarkers = markers.map(({ lat, lng }) =>
+  L.marker([lat, lng]).addTo(map)
+)
 
-let currentMarker = marker1
+leafletMarkers[1].bindPopup("<b>Otro Lorem Ipsum</b><br>Maecenas euismod cursus dignissim.")
+
+let currentMarkerIndex = 0
 const animateButton = document.getElementById("animateButton")
 animateButton.addEventListener("click", () => {
-  if (currentMarker === marker1) {
-    map.flyTo([marker2Lat, marker2Lng], 15)
-    currentMarker = marker2
-    animateButton.textContent = "Ir a Marker 1"
-  } else {
-    map.flyTo([marker1Lat, marker1Lng], zoom)
-    currentMarker = marker1
-    animateButton.textContent = "Ir a Marker 2"
-  }
+  currentMarkerIndex = (currentMarkerIndex + 1) % markers.length
+  const currentMarker = leafletMarkers[currentMarkerIndex]
+  map.flyTo([currentMarker.getLatLng().lat, currentMarker.getLatLng().lng], 15)
+  animateButton.textContent = `Ir a Marker ${currentMarkerIndex + 1}`
 })
