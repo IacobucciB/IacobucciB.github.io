66bc2d77f92f532216b50215883f21ae2fde1f86
IacobucciB <link00222@gmail.com>
2023-07-06 18:00:16 -0300

    Leaf

--- leaf/script.js
+++ leaf/script.js
@@ -1,32 +1,71 @@
 let config = {
   minZoom: 7,
   maxZoom: 18,
-}
-const zoom = 12
+};
 
-const markers = [
-  { lat: -34.922883, lng: -57.956317 },
-  { lat: -54.801912, lng: -68.303216 },
-]
+const zoom = 12;
 
-const map = L.map("map", config).setView([markers[0].lat, markers[0].lng], zoom)
+const json = {
+  "america_del_norte": {
+    "universidad": "Universidad de Stanford",
+    "latitud": 37.4275,
+    "longitud": -122.1697,
+    "descripcion": "La Universidad de Stanford es una institución líder en investigación y educación, reconocida mundialmente por su excelencia académica y sus contribuciones en diversos campos como la tecnología, la medicina y las ciencias sociales."
+  },
+  "america_del_sur": {
+    "universidad": "Universidad de Buenos Aires",
+    "latitud": -34.6037,
+    "longitud": -58.3816,
+    "descripcion": "La Universidad de Buenos Aires es la institución educativa más prestigiosa de América del Sur, destacada por su calidad académica y su amplia oferta de programas de estudio en diversas disciplinas."
+  },
+  "europa": {
+    "universidad": "Universidad de Oxford",
+    "latitud": 51.754816,
+    "longitud": -1.254367,
+    "descripcion": "La Universidad de Oxford, una de las universidades más antiguas del mundo, es reconocida por su excelencia académica y su compromiso con la investigación de vanguardia en campos como las ciencias, las humanidades y las ciencias sociales."
+  },
+  "africa": {
+    "universidad": "Universidad de Ciudad del Cabo",
+    "latitud": -33.9575,
+    "longitud": 18.461,
+    "descripcion": "La Universidad de Ciudad del Cabo es una institución de renombre en África, conocida por su excelencia en la educación superior, su enfoque en la diversidad cultural y su contribución a la investigación científica y social en el continente."
+  },
+  "asia": {
+    "universidad": "Universidad de Tokio",
+    "latitud": 35.7131,
+    "longitud": 139.7628,
+    "descripcion": "La Universidad de Tokio es una de las principales instituciones educativas de Asia, reconocida por su excelencia en la enseñanza y la investigación en una amplia gama de disciplinas, incluyendo la ciencia, la tecnología, las humanidades y las ciencias sociales."
+  },
+  "oceania": {
+    "universidad": "Universidad de Melbourne",
+    "latitud": -37.7964,
+    "longitud": 144.9612,
+    "descripcion": "La Universidad de Melbourne es una institución destacada en Oceanía, conocida por su calidad académica y su compromiso con la investigación innovadora. Ofrece una amplia variedad de programas de estudio y cuenta con reconocidos académicos en diversas áreas del conocimiento."
+  }
+};
+
+const markers = Object.values(json).map(({ latitud, longitud, descripcion }) => ({
+  lat: latitud,
+  lng: longitud,
+  descripcion: descripcion
+}));
+
+const map = L.map("map", config).setView([markers[0].lat, markers[0].lng], zoom);
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map)
+}).addTo(map);
 
-const leafletMarkers = markers.map(({ lat, lng }) =>
-  L.marker([lat, lng]).addTo(map)
-)
+const leafletMarkers = markers.map(({ lat, lng, descripcion }) =>
+  L.marker([lat, lng]).addTo(map).bindPopup(descripcion)
+);
 
-leafletMarkers[1].bindPopup("<b>Otro Lorem Ipsum</b><br>Maecenas euismod cursus dignissim.")
-
-let currentMarkerIndex = 0
-const animateButton = document.getElementById("animateButton")
+let currentMarkerIndex = 0;
+const animateButton = document.getElementById("animateButton");
 animateButton.addEventListener("click", () => {
-  currentMarkerIndex = (currentMarkerIndex + 1) % markers.length
-  const currentMarker = leafletMarkers[currentMarkerIndex]
-  map.flyTo([currentMarker.getLatLng().lat, currentMarker.getLatLng().lng], 15)
-  animateButton.textContent = `Ir a Marker ${currentMarkerIndex + 1}`
-})
+  currentMarkerIndex = (currentMarkerIndex + 1) % markers.length;
+  const currentMarker = leafletMarkers[currentMarkerIndex];
+  map.flyTo([currentMarker.getLatLng().lat, currentMarker.getLatLng().lng], 15);
+  animateButton.textContent = `Ir a ${json[Object.keys(json)[currentMarkerIndex]].universidad}`;
+});
