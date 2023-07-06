6753d1bceefb0f51eb5190bb64ce487a4f4de3ed
IacobucciB <link00222@gmail.com>
2023-07-06 18:35:16 -0300

    Sync

--- leaf/script.js
+++ leaf/script.js
@@ -10,37 +10,43 @@ const json = {
     "universidad": "Universidad de Stanford",
     "latitud": 37.4275,
     "longitud": -122.1697,
-    "descripcion": "La Universidad de Stanford es una institución líder en investigación y educación, reconocida mundialmente por su excelencia académica y sus contribuciones en diversos campos como la tecnología, la medicina y las ciencias sociales."
+    "descripcion": "La Universidad de Stanford es una institución líder en investigación y educación, reconocida mundialmente por su excelencia académica y sus contribuciones en diversos campos como la tecnología, la medicina y las ciencias sociales.",
+    "imagen": "img/STA.jpg"
   },
   "america_del_sur": {
     "universidad": "Universidad de Buenos Aires",
     "latitud": -34.6037,
     "longitud": -58.3816,
-    "descripcion": "La Universidad de Buenos Aires es la institución educativa más prestigiosa de América del Sur, destacada por su calidad académica y su amplia oferta de programas de estudio en diversas disciplinas."
+    "descripcion": "La Universidad de Buenos Aires es la institución educativa más prestigiosa de América del Sur, destacada por su calidad académica y su amplia oferta de programas de estudio en diversas disciplinas.",
+    "imagen": "img/UBA.jpg"
   },
   "europa": {
     "universidad": "Universidad de Oxford",
     "latitud": 51.754816,
     "longitud": -1.254367,
-    "descripcion": "La Universidad de Oxford, una de las universidades más antiguas del mundo, es reconocida por su excelencia académica y su compromiso con la investigación de vanguardia en campos como las ciencias, las humanidades y las ciencias sociales."
+    "descripcion": "La Universidad de Oxford, una de las universidades más antiguas del mundo, es reconocida por su excelencia académica y su compromiso con la investigación de vanguardia en campos como las ciencias, las humanidades y las ciencias sociales.",
+    "imagen": "img/OXF.jpg"
   },
   "africa": {
     "universidad": "Universidad de Ciudad del Cabo",
     "latitud": -33.9575,
     "longitud": 18.461,
-    "descripcion": "La Universidad de Ciudad del Cabo es una institución de renombre en África, conocida por su excelencia en la educación superior, su enfoque en la diversidad cultural y su contribución a la investigación científica y social en el continente."
+    "descripcion": "La Universidad de Ciudad del Cabo es una institución de renombre en África, conocida por su excelencia en la educación superior, su enfoque en la diversidad cultural y su contribución a la investigación científica y social en el continente.",
+    "imagen": "img/CAB.jpg"
   },
   "asia": {
     "universidad": "Universidad de Tokio",
     "latitud": 35.7131,
     "longitud": 139.7628,
-    "descripcion": "La Universidad de Tokio es una de las principales instituciones educativas de Asia, reconocida por su excelencia en la enseñanza y la investigación en una amplia gama de disciplinas, incluyendo la ciencia, la tecnología, las humanidades y las ciencias sociales."
+    "descripcion": "La Universidad de Tokio es una de las principales instituciones educativas de Asia, reconocida por su excelencia en la enseñanza y la investigación en una amplia gama de disciplinas, incluyendo la ciencia, la tecnología, las humanidades y las ciencias sociales.",
+    "imagen": "img/TOK.jpg"
   },
   "oceania": {
     "universidad": "Universidad de Melbourne",
     "latitud": -37.7964,
     "longitud": 144.9612,
-    "descripcion": "La Universidad de Melbourne es una institución destacada en Oceanía, conocida por su calidad académica y su compromiso con la investigación innovadora. Ofrece una amplia variedad de programas de estudio y cuenta con reconocidos académicos en diversas áreas del conocimiento."
+    "descripcion": "La Universidad de Melbourne es una institución destacada en Oceanía, conocida por su calidad académica y su compromiso con la investigación innovadora. Ofrece una amplia variedad de programas de estudio y cuenta con reconocidos académicos en diversas áreas del conocimiento.",
+    "imagen": "img/MEL.jpg"
   }
 };
 
